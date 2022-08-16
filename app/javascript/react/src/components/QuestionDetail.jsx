ea215e077479b99c400a9f8e6112b8551dac4781
Ravi Singh <traning.appsimpact@gmail.com>
2022-08-16 16:41:47 +0530

    added dislike button

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -6,15 +6,27 @@ class QuestionDetail extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { likeCount: 0 }
+    this.state = { 
+      likeCount: 0,
+      dislikeCount: 0
+    }
 
-    this.updateLikeCounter = this.updateLikeCounter.bind(this)  
+    this.updateLikeCounter = this.updateLikeCounter.bind(this)
+    this.updateDislikeCounter = this.updateDislikeCounter.bind(this)  
   }
 
   updateLikeCounter = () => {
     this.setState((state) => {
       return {
         likeCount: state.likeCount + 1
+      }
+    })
+  }
+
+  updateDislikeCounter = () => {
+    this.setState((state) => {
+      return {
+        dislikeCount: state.dislikeCount + 1
       }
     })
   }
@@ -27,10 +39,19 @@ class QuestionDetail extends React.Component {
           <p className="lead">
             <span className="badge bg-primary">{this.props.question.tag}</span>
           </p>
-          <button className="btn btn-primary mt-1" onClick={this.updateLikeCounter}>Like</button>
-          { this.state.likeCount > 0 ?
-            <span className="badge bg-primary">{this.state.likeCount}</span> : ''
-          }
+          <button type="button" className="btn btn-primary position-relative" onClick={this.updateLikeCounter} style={{marginRight: 1 + 'em'}}>
+            Like
+            { this.state.likeCount > 0 ?
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{this.state.likeCount}</span> : ''
+            }
+          </button>
+
+          <button type="button" className="btn btn-primary position-relative" onClick={this.updateDislikeCounter}>
+            Dislike
+            { this.state.dislikeCount > 0 ?
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{this.state.dislikeCount}</span> : ''
+            }
+          </button>
         </div>
       </div>
     )
