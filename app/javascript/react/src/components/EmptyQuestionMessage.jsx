ba0558c9cf117a237b9f884dbe3c5e75bdba97e7
Ravi Singh <traning.appsimpact@gmail.com>
2022-08-20 13:34:06 +0530

    fixed alert close issue

--- app/javascript/react/src/components/EmptyQuestionMessage.jsx
+++ app/javascript/react/src/components/EmptyQuestionMessage.jsx
@@ -3,9 +3,11 @@ import * as ReactDOM from 'react-dom'
 
 const EmptyQuestionMessage = (props) => {
   return(
-    <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>OOPs!</strong> No questions found with the tag: {props.tagname}. Please select another options form the list.
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div>
+      <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>OOPs!</strong> No questions found with the tag: {props.tagname}. Please select another options form the list.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   )
 }
