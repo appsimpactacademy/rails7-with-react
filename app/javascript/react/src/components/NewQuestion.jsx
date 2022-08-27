b4ddd15ab328799c27f4de20fa4ae7c2f3d0b5c9
Ravi Singh <traning.appsimpact@gmail.com>
2022-08-27 12:13:47 +0530

    display server side errors on react forms

--- app/javascript/react/src/components/NewQuestion.jsx
+++ app/javascript/react/src/components/NewQuestion.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
 import * as ReactDOM from 'react-dom'
+import ServerSideError from './ServerSideError'
 
 const NewQuestion = () => {
   const questionsTags = [
@@ -22,6 +23,9 @@ const NewQuestion = () => {
   // const handleTagChange = (event) => {
   //   setTag(event.target.value)
   // }
+
+  const [isServerSideError, setIsServerSideError] = useState(false)
+  const [serverErrors, setServerErrors] = useState([])
 
   const [formField, setFormField] = useState({
     title: '',
@@ -49,6 +53,13 @@ const NewQuestion = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log('Success:', data)
+      if(data['status'] === "failure") {
+        setIsServerSideError(true)
+        setServerErrors(data['data'])
+      } else {
+        setIsServerSideError(false)
+        setServerErrors([])
+      }
     })
     .catch((error) => {
       console.log('Error:', error)
@@ -65,6 +76,7 @@ const NewQuestion = () => {
           </div>
           <form onSubmit={handleQuestionSubmit}>
             <div className="modal-body">
+              { isServerSideError && <ServerSideError errors={serverErrors}/> }
               <div className="form-group">
                 <label className="form-label mt-3 mb-3">Title</label>
                 <input type="text" className="form-control form-control-lg rounded-0" value={formField.title} onChange={event => handleFormFields(event)} name="title" />
---
+++ app/javascript/react/src/components/ServerSideError.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import * as ReactDOM from 'react-dom'
+
+const ServerSideError = (props) => {
+  return(
+    <>
+      <p className="lead fw-bold">Please fix the errors below</p>
+      { props.errors.map((error, index) => (
+        <p className="text-danger" key={index}>{error}</p>
+      )) }
+    </>
+  )
+}
+
+export default ServerSideError
