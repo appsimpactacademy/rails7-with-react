ba0f2a2be6631b096e322cff4440a7a4c4330149
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-09-05 17:21:01 +0200

    Replace sqlite3 with postgreSQL DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_18_074238) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.string "tag"
